8326b6e2666d3e6b34a14474c7b52fb1c269aeef
Kalana Denuz <kalanadenuz@gmail.com>
2026-01-17 10:27:23 +0530

    day 3

---
+++ 01-Fullstack/Practice/javascript/variables.js
@@ -0,0 +1,26 @@
+/* let name = "denu";
+
+let a = name.toUpperCase();
+let b = name.includes("k");
+let c = name.slice(0,2);
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+let age = 16;
+let d = Math.sqrt(age);
+console.log(d);
+
+let num="50";
+
+let e = num -1;
+
+console.log(e);
+*/
+
+let u1=user;
+
+u1.age = 30;
+
+console.log(u1.age);
