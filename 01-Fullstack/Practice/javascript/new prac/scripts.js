30fcdbc5c896c27e7b5c10b1a327b6707f4649cf
Kalana Denuz <kalanadenuz2002@gmail.com>
2026-01-21 22:05:34 +0530

    add text stylings

--- 01-Fullstack/Practice/javascript/new prac/scripts.js
+++ 01-Fullstack/Practice/javascript/new prac/scripts.js
@@ -1,23 +1,21 @@
-let people =[];
-function Person(name,id,age,weight,height,iq){
-    this.name=name;
-    this.id =id;
-    this.age=age;
-    this.weight=weight;
-    this.height=height;
-    this.iq=iq;
-    people.push(this);
-    
-};
+let people = [];
 
+class Person {
+    constructor(name, id, age, weight, height, iq) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.iq = iq;
 
+        
+        people.push(this);
+    }
+   
+}
 
-let p1 = new Person("Kalana",12,23,234,32,1);
+let p1 = new Person("Kalana", 12, 23, 234, 32, 1);
+let p2 = new Person("Tiki", 12, 23, 234, 32, 1);
 
-console.log(people);
-
-
-
-
-
-
+console.log("People: ", people);
